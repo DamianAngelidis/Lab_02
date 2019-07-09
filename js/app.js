50f553c8b95519b8bbb49f1e3dccde57723b135d
Damian Michael Angelidis <damianangelidis@yahoo.com>
2019-07-09 14:41:58 -0700

    I have coded three of the questions in javascript. About me section in html with a link in the table of contents.

---
+++ js/app.js
@@ -0,0 +1,96 @@
+var properAnswer1 = false;
+
+while(properAnswer1 === false) { 
+
+    var writesPoems =  prompt('Does Damian writes poetry (y/n)');
+
+    if (writesPoems.toLowerCase() === 'yes' || writesPoems.toLowerCase() === 'y') {
+        alert('You are correct! It\'s been a hobby and a habit of mine since 5th grade.');
+        properAnswer1 = true;
+    }
+    else if (writesPoems.toLowerCase() === 'no' || writesPoems.toLowerCase() === 'n') {
+        alert('Actually, I loving writing poetry, and have been doing it since the 5th grade');
+        properAnswer1 = true;
+    }
+    else {
+        alert('It seems that you didn\'t input an acceptable answer. To answer yes, type "yes" or just "y". For no, "no" or "n" will suffice.')
+    }
+
+}
+console.log('The user answered ' + writesPoems + ' when asked whether or not Damian wrote poetry')
+var properAnswer2 = false;
+
+while(properAnswer2 === false) { 
+
+    var writesPoems =  prompt('Is New York style pizza Damian\'s favorite style of pizza? (y/n)');
+
+    if (writesPoems.toLowerCase() === 'yes' || writesPoems.toLowerCase() === 'y') {
+        alert('You absolute buffoon. How dare you put that disgusting slice above the holy pie that is Chicago deep dish. I would say there is a special place in ell for your kind, but New York already is that place.');
+        properAnswer2 = true;
+    }
+    else if (writesPoems.toLowerCase() === 'no' || writesPoems.toLowerCase() === 'n') {
+        alert('You, my fellow person of culture, are correct. Us Chicago deep dish purists must stick together to defend against the New York style heathens');
+        properAnswer2 = true;
+    }
+    else {
+        alert('It seems that you didn\'t input an acceptable answer. To answer yes, type "yes" or just "y". For no, "no" or "n" will suffice.')
+    }
+
+}
+
+
+var properAnswer3 = false;
+
+while(properAnswer3 === false) { 
+
+    var writesPoems =  prompt('Does Damian have a dog? (y/n)');
+
+    if (writesPoems.toLowerCase() === 'yes' || writesPoems.toLowerCase() === 'y') {
+        alert('According to the judge, I am legally required to refer to Beau as a dog. Colloquioally, I prefer to address him as a furry spawn of pure evil whose cold, void-like, and empty eyes hide his singular goal to ruin my life by any means neccesary.');
+    }
+    else if (writesPoems.toLowerCase() === 'no' || writesPoems.toLowerCase() === 'n') {
+        alert('If only. Unfortuantely, I am stuck in my personal, agonizing limbo with Beau, and as each day goes by, I become more fearful that he is immortal. He has consumed all that was good in my life, but refuses to end me. Instead he just sits there, taunting me with his tongue out, staring deep into my now empty soul. At least I don\'t have a cat.');
+    }
+    else {
+        alert('It seems that you didn\'t input an acceptable answer. To answer yes, type "yes" or just "y". For no, "no" or "n" will suffice.')
+    }
+
+}
+
+
+var properAnswer4 = false;
+
+while(properAnswer4 === false) { 
+
+    var writesPoems =  prompt('Do you think Damian writes poetry (y/n)');
+
+    if (writesPoems.toLowerCase() === 'yes' || writesPoems.toLowerCase() === 'y') {
+        alert('You are correct! It\'s been a hobby and a habit of mine since 5th grade.');
+    }
+    else if (writesPoems.toLowerCase() === 'no' || writesPoems.toLowerCase() === 'n') {
+        alert('Actually, I loving writing poetry, and have been doing it since the 5th grade');
+    }
+    else {
+        alert('It seems that you didn\'t input an acceptable answer. To answer yes, type "yes" or just "y". For no, "no" or "n" will suffice.')
+    }
+
+}
+
+
+var properAnswer5 = false;
+
+while(properAnswer5 === false) { 
+
+    var writesPoems =  prompt('Do you think Damian writes poetry (y/n)');
+
+    if (writesPoems.toLowerCase() === 'yes' || writesPoems.toLowerCase() === 'y') {
+        alert('You are correct! It\'s been a hobby and a habit of mine since 5th grade.');
+    }
+    else if (writesPoems.toLowerCase() === 'no' || writesPoems.toLowerCase() === 'n') {
+        alert('Actually, I loving writing poetry, and have been doing it since the 5th grade');
+    }
+    else {
+        alert('It seems that you didn\'t input an acceptable answer. To answer yes, type "yes" or just "y". For no, "no" or "n" will suffice.')
+    }
+
+}
