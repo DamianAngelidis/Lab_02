6319f7d1dc10f97bccf186c69bb218f86b78f6a7
SteampunkMagic <damianangelidis@yahoo.com>
2019-08-05 12:34:25 -0700

    Final push

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
 'use strict';
 
 var modal = document.getElementById("cover");
+modal.addEventListener('click', handleModalChoice);
+// var noModal = document.getElementById('alert');
+// noModal.addEventListener('click', handleAlertChoice);
+var yesModal = document.getElementById('questionnaire');
 //Close the Modal
+
+function handleModalChoice(event) {
+    event.preventDefault();
+
+    console.log(event.target.class, event.target, event.target.textContent);    
+    
+    if(event.target.class = 'yesquestions') {
+        modal.style.display = "none";
+        yesModal.style.display = 'block';
+        console.log('Answer Yes');
+    }
+    else if(event.target.class = 'noquestions') {
+        modal.style.display = "none";
+        // noModal.style.display = 'block';
+        console.log('Answer No');
+    }
+    console.log(event.target.class, event.target, event.target.textContent);
+
+}
+
+// function handleAlertChoice(event) {
+//     event.preventDefault();
+//     console.log('Im here')
+    
+//     if(event.target.class = 'yesquestions') {
+//         modal.style.display = "none";
+//         yesModal.style.display = 'block';
+//     }
+//     if(event.target.class = 'noquestions') {
+//         noModal.style.display = "none";
+//     }
+
+// }
 
 function getToHome() {
     window.location = 'index.html';
@@ -13,15 +50,10 @@ function getToAbout() {
     console.log(window.location);
 }
 
-function closeModal() {
-    modal.style.display = "none";
-}
-
 //Ask the person the questions
 function questions(){
     console.log(questions)
-      
-var questionAsk = ['Do I write Poetry?','Is my favorite type of pizza NY style?','Do I have a dog?','Am I alive?','Am I Batman?'];
+
 var answerQuest = ['yes','no','yes','no','no','yes','yes','no','no','yes'];
 var correctResponse = ['You are correct! It\'s been a hobby and a habit of mine since 5th grade.','You absolute buffoon. How dare you put that disgusting slice above the holy pie that is Chicago deep dish. I would say there is a special place in ell for your kind, but New York already is that place.','If only. Unfortuantely, I am stuck in my personal, agonizing limbo with Beau, and as each day goes by, I become more fearful that he is immortal. He has consumed all that was good in my life, but refuses to end me. Instead he just sits there, taunting me with his tongue out, staring deep into my now empty soul. At least I don\'t have a cat.','hahahahahahahahahahahahahahahahah. Who knows?','You\'re right, of course. I just find it a little hurtful that you didn\'t even consider that I could be. I knew it was unfunny and desperate when I wrote it, I guess I was just hoping that you would humor me for the sake of support. Guess not'];
 var wrongResponse = ['Actually, I loving writing poetry, and have been doing it since the 5th grade','You, my fellow person of culture, are correct. Us Chicago deep dish purists must stick together to defend against the New York style heathens','According to the judge, I am legally required to refer to Beau as a dog. Colloquioally, I prefer to address him as a furry spawn of pure evil whose cold, void-like, and empty eyes hide his singular goal to ruin my life by any means neccesary.','hahahahahahahahahahahahahahahahah. It\'s all a simulation.','What? Of course he\'s not Batman. Here are the question YOU need to be asking: What led you to believe that a real person could be a fictional character How bad is Damian\'s sense of humour, that he actually wrote that question?'];  
@@ -44,7 +76,7 @@ for(var i=0;i<questionAsk.length;i++){
         if(forAns === answerQuest[i*2]){
             alert(correctResponse[i]); 
             properAnswer = true;
-            correctAnswer++;
+            correctAnswer++;    
 
             console.log(correctResponse[i],properAnswer);
 
@@ -87,7 +119,7 @@ for(var i=0;i<questionAsk.length;i++){
 
     //If the user did not get the right answer, tell them
     if(correctGuess === false && numGuess === 3) {
-        alert('The correct was 13. THat\'s right I\'ve had 13 ear infections');
+        alert('The correct answer was 13. Thbat\'s right I\'ve had 13 ear infections');
     }
 
     properAnswer = false;
@@ -103,10 +135,10 @@ for(var i=0;i<questionAsk.length;i++){
 
             console.log(favCountries[i])
              
-            if(y.toLowerCase() === favCountries[i].toLowerCase()) {
+            if(guess.toLowerCase() === favCountries[i].toLowerCase()) {
 
                 console.log(guess, favCountries[i]);
-                correctGuess2 = true;
+                properAnswer = true;
                 console.log(correctGuess);
                 alert('That\'s correct! ' + favCountries[i] + ' is one of my favorite countries.')
                 correctAnswer = correctAnswer + 1;
@@ -118,7 +150,7 @@ for(var i=0;i<questionAsk.length;i++){
         if(properAnswer === false ){
 
             numGuess++;
-            alert('While it is interesting, ' + y + ' is not one of my countries You have ' + (6 - numGuess2) + ' guesses left.');
+            alert('While it is interesting, ' + guess + ' is not one of my countries You have ' + (6 - numGuess) + ' guesses left.');
         
         }
     }
@@ -132,12 +164,5 @@ for(var i=0;i<questionAsk.length;i++){
     alert('Good job finishing the guessing game. You got ' + correctAnswer + ' questions right out of 7. ');
     modal.style.display = "none";
 
-}
-
-document.getElementById("homebutton").onclick = function() {
-    getToHome();
-}
-document.getElementById("aboutmebutton").onclick = function() {
-    getToAbout();
 }
 
